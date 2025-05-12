eb90e1a841620740045ef794dd58df78344a9b0f
Du Due <150822590+Heeee-DuDue@users.noreply.github.com>
2025-05-12 15:23:39 +0800

    Update TupleSpaceServer.java

--- TupleSpaceServer.java
+++ TupleSpaceServer.java
@@ -14,6 +14,45 @@ public class TupleSpaceServer {
     private static final AtomicInteger totalPuts = new AtomicInteger(initialValue:0);
     private static final AtomicInteger totalErrors = new AtomicInteger(initialValue:0);
 
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java TupleSpaceServer <port>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+        // Output statisticial information every 10 seconds
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @override
+            public void run() {
+                int tuples = tupleSpace.size();
+                int clients = totalClients.get();
+                int ops = totalOperations.get();
+                int reads = totalReads.get();
+                int gets = totalGets.get();
+                int puts = totalPuts.get();
+                int errors = totalErrors.get();
+
+                System.out.printf(
+                    format:"[STATS] Tuples=%d, Clients=%d, Ops=%d (READ=%d, GET=%d, PUT=%d), Errors=%d%n",
+                    tuples, clients, ops, reads, gets, puts, errors
+                );
+            }
+        }, delay:0, period:10000);
+
+        // Accept client connections
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            totalClients.incrementAndGet();
+            threadPool.execute(new ClientHandler(clientSocket));
+        }
+    }
+
+
     
     
 
