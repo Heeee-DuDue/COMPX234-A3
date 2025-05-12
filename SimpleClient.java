12d7e4139a07c06f92ae2eddd10a6264c81bcaa7
Du Due <150822590+Heeee-DuDue@users.noreply.github.com>
2025-05-12 16:40:07 +0800

    Update SimpleClient.java

--- SimpleClient.java
+++ SimpleClient.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.net.*;
 
+public class SimpleClient {
+  public static void main(String[] args) throws IOExceptions {
+    Socket socket = new Socket ("localhost", 51234);
+    System.out.println("Connected to server.");
+
+    try (
+      BufferedReader in = new BufferedReader(
+        new inputStreamReader(socket.getOutputStream(), true);
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader console = new BufferedReader(
+        new InputStreamReader(System.in)
+      )
+    ){
+      String userInput;
+      while ((userInput = console.readLine()) != null) {
+        out.println(userInput); // Send a message to the server
+        String response = in.readLine(); // Receive server response
+        System.out.println("Server:" + response);
+      }
+    }
+  }
+}
