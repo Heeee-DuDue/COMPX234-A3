b488d9e68725a6a1e97f8a493ef7fb4f586e5062
Du Due <150822590+Heeee-DuDue@users.noreply.github.com>
2025-05-12 20:22:17 +0800

    Update SimpleClient.java

--- SimpleClient.java
+++ SimpleClient.java
@@ -21,7 +21,7 @@ public class TupleSpaceClient {
             String line;
             while ((line = fileReader.readLine()) != null) {
                 String[] parts = line.split(regex:" ", limit:3);
-                if (parts.length < 2) {
+                if (parts.length < 2) {x`
                     system.err.println("Invalid request: " + line);
                     continue;
                 }
@@ -30,7 +30,37 @@ public class TupleSpaceClient {
                 String key = parts[1];
                 String value = (parts.length > 2) ? parts[2] : "";
 
-                //
+                // Construct protocol messages
+                String protocolOp;
+                String requestBody;
+                switch (op) {
+                    case "PUT" :
+                        protocolOp = "P";
+                        requestBody = protocolOp + " " + key + " " + value;
+                        break;
+                    case "GET" :
+                        protocolOp = "G";
+                        requestBody = protocolOp + " " + key;
+                        break;
+                    case "READ" :
+                        protocolOp = "R";
+                        requestBody = protocolOp + " " + key;
+                        break;
+                    default:
+                        System.err.println("Unknown operation: " + op);
+                        continue;
+                }
+
+                // Calculate message length (NNN) 
+                int length = requestBody.length() + 4; //3-digit length+space
+                String formattedRequest = String.format("%03d %s", length, requestBody);
+
+                // Send requests and receive responses
+                out.println(formattedRequest);
+                String response = in.readline();
+                System.out.println(line + ": " + response);
+            }
+        } catch 
 
 
                 
